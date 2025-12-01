4e78e923fdf1630a38a0bc295d2274d9e4132688
leeseyeonn <zzin8520@gmail.com>
2025-12-01 18:19:54 +0900

    2행

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* 슬라이드 배너 */
+/* ----------------------------------슬라이드 배너 */
 
 const row1Bnr = new Swiper('#main_banner', { 
     loop:true,
@@ -12,12 +12,77 @@ const row1Bnr = new Swiper('#main_banner', {
     },
 })
 
-/* 신상품 배너 */
+/* -------------------------------------신상품 */
 
-const row2Bnr = new Swiper('#new_item', { 
+/* solorcore - all */
+const row2Bnr = new Swiper('#solarcore_all', { 
+    slidesPerView:5.8,
     loop:true,
     navigation: {
-        nextEl: "#new_item .swiper-button-next",
-        prevEl: "#new_item .swiper-button-prev",
+        nextEl: "#solarcore_all .swiper-button-next",
+        prevEl: "#solarcore_all .swiper-button-prev",
+    },
+})
+
+
+/* ------------------------------------- 콜렉션 */
+/* women */
+
+const row3Bnr = new Swiper('#collect_women', { 
+    /* slidesPerView:5.8, */
+    loop:true,
+    navigation: {
+        nextEl: "#collect_women .swiper-button-next",
+        prevEl: "#collect_women .swiper-button-prev",
+    },
+})
+
+/* ------------------------------------- 신발 */
+
+const row4Bnr = new Swiper('#shoes', { 
+    /* slidesPerView:5.8, */
+    loop:true,
+    navigation: {
+        nextEl: "#shoes .swiper-button-next",
+        prevEl: "#shoes .swiper-button-prev",
+    },
+})
+
+/* ------------------------------------- 이글스 */
+
+/* 상품 슬라이드 */
+
+const row7List = new Swiper('#eagles_list', { 
+    /* slidesPerView:5.8, */
+    loop:true,
+    navigation: {
+        nextEl: "#eagles_list .swiper-button-next",
+        prevEl: "#eagles_list .swiper-button-prev",
+    },
+})
+
+
+/* 공지배너 */
+const row7Bnr = new Swiper('#eagles_bnr', { 
+    /* slidesPerView:5.8, */
+    loop:true,
+    pagination: {
+        el: "#eagles_bnr .swiper-pagination",
+    },
+    navigation: {
+        nextEl: "#eagles_bnr .swiper-button-next",
+        prevEl: "#eagles_bnr .swiper-button-prev",
+    },
+})
+
+
+/* ------------------------------------- 빅펀치 */
+
+const row8List = new Swiper('#big_punch_list', { 
+    /* slidesPerView:5.8, */
+    loop:true,
+    navigation: {
+        nextEl: "#big_punch_list .swiper-button-next",
+        prevEl: "#big_punch_list .swiper-button-prev",
     },
 })
